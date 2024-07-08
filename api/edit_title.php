4b1cf4409a59cd9492233c5ba5e38c50379bdd27
kai~ <test@gmail.com>
2024-07-08 14:54:48 +0800

    小修改''

--- api/edit_title.php
+++ api/edit_title.php
@@ -8,9 +8,9 @@ foreach ($_POST['id'] as $key => $id) {
         $pdo->exec($sql);
     } else {
         if (isset($_POST['sh']) && $_POST['sh'] == $id) {
-            $sql = "update `title` set `text` = '{$_POST['text'][$key]}', `sh`='1' where id=$id'";
+            $sql = "update `title` set `text` = '{$_POST['text'][$key]}', `sh`='1' where id='$id'";
         } else {
-            $sql = "update `title` set `text` = '{$_POST['text'][$key]}', `sh`='0' where id=$id'";
+            $sql = "update `title` set `text` = '{$_POST['text'][$key]}', `sh`='0' where id='$id'";
         }
         $pdo->exec($sql);
     }
